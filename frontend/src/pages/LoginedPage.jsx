4d68d1e63bab6e1408945f637b418a854c9fbcad
Ryan <2436566282@qq.com>
2026-09-17 23:41:20 +0800

    v1.1.1 名称bug

--- frontend/src/pages/LoginedPage.jsx
+++ frontend/src/pages/LoginedPage.jsx
@@ -4,7 +4,7 @@ import { Button, Typography, Space } from 'antd';
 import { UserOutlined, LogoutOutlined, HomeOutlined } from '@ant-design/icons';
 import AuthCard from '../components/AuthCard';
 import BackgroundGlow from '../components/BackgroundGlow';
-import './LoginedPage.css';   // ← 加上这行
+import './loginedpage.css';   // ← 加上这行
 
 const { Title, Text } = Typography;
 
